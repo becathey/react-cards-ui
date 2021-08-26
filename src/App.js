c00285dad22c28493305bf332e302e55fc38a4e3
Bill Cathey <billcathey@hotmail.com>
2021-08-26 17:49:48 -0400

    Add and render basic Card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import "./sass/App.scss";
+import image01 from "./images/image01.png";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello World!</h1>
+      <Card />
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <img src={image01} alt="" className="card-image" />
+      <div className="card-body">
+        <h2 className="card-title">Title</h2>
+        <p className="card-description">Description</p>
+        <h3 className="card-price">00.00</h3>
+        <button className="card-btn">Add to Cart</button>
+      </div>
     </div>
   );
 }
